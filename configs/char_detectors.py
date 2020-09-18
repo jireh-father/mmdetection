e74d81ee6b68c1c9824cc7ccecbbc620042de497
seoilgun <seoilgun@gmail.com>
2020-09-18 23:54:24 +0900

    added custom datasets

--- configs/char_detectors.py
+++ configs/char_detectors.py
@@ -32,7 +32,7 @@ model = dict(
             style='pytorch')))
 
 work_dir = './work_dirs/char_detectors'
-fp16 = dict(loss_scale=512.)
+# fp16 = dict(loss_scale=512.)
 
 optimizer = dict(type='SGD', lr=0.0002, momentum=0.9, weight_decay=0.0001)
 gpus = 2
