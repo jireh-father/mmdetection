eb71c73da9f3189649044d348fedbb103914f19d
seoilgun <seoilgun@gmail.com>
2020-09-18 12:59:48 +0900

    added custom datasets

--- configs/char_detectors.py
+++ configs/char_detectors.py
@@ -1,4 +1,8 @@
-_base_ = './htc/htc_r50_fpn_1x_char.py'
+_base_ = [
+    '../_base_/models/cascade_mask_rcnn_r50_fpn_char.py',
+    '../_base_/datasets/char_instance.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
 
 model = dict(
     backbone=dict(
