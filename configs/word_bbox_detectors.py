5946ca35953bb5487ada4748f710a18830b261e6
seoilgun <seoilgun@gmail.com>
2020-09-19 17:55:08 +0900

    added custom datasets

---
+++ configs/word_bbox_detectors.py
@@ -0,0 +1,37 @@
+_base_ = [
+    './_base_/models/cascade_mask_rcnn_r50_fpn_word.py',
+    './_base_/datasets/word_bbox_instance.py',
+    './_base_/schedules/schedule_1x.py', './_base_/default_runtime.py'
+]
+
+model = dict(
+    backbone=dict(
+        type='DetectoRS_ResNet',
+        conv_cfg=dict(type='ConvAWS'),
+        sac=dict(type='SAC', use_deform=True),
+        stage_with_sac=(False, True, True, True),
+        output_img=True),
+    neck=dict(
+        type='RFP',
+        rfp_steps=2,
+        aspp_out_channels=64,
+        aspp_dilations=(1, 3, 6, 1),
+        rfp_backbone=dict(
+            rfp_inplanes=256,
+            type='DetectoRS_ResNet',
+            depth=50,
+            num_stages=4,
+            out_indices=(0, 1, 2, 3),
+            frozen_stages=1,
+            norm_cfg=dict(type='BN', requires_grad=True),
+            norm_eval=True,
+            conv_cfg=dict(type='ConvAWS'),
+            sac=dict(type='SAC', use_deform=True),
+            stage_with_sac=(False, True, True, True),
+            pretrained='torchvision://resnet50',
+            style='pytorch')))
+
+work_dir = './work_dirs/word_bbox_detectors'
+# fp16 = dict(loss_scale=512.)
+
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
