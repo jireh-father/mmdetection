6936205f112e30604a21a2d1e060c04d558cc52f
seoilgun <seoilgun@gmail.com>
2020-09-20 02:04:31 +0900

    ~

--- demo/images_demo.py
+++ demo/images_demo.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 from mmdet.apis import inference_detector, init_detector, save_result_pyplot
 import glob
 import os
+import time
 
 
 def main():
@@ -24,7 +25,9 @@ def main():
     img_files = glob.glob(args.imgs)
     for img in img_files:
         # test a single image
+        start = time.time()
         result = inference_detector(model, img)
+        print(time.time() - start)
         # show the results
         output_path = os.path.join(args.output_dir, os.path.splitext(os.path.basename(img))[0] + ".jpg")
         save_result_pyplot(model, img, result, output_path, score_thr=args.score_thr)
