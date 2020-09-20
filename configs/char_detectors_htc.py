f689f340b6256dc6ed9cd5ee01c39aad81149a48
seoilgun <seoilgun@gmail.com>
2020-09-21 08:36:32 +0900

    ~

--- configs/char_detectors_htc.py
+++ configs/char_detectors_htc.py
@@ -30,4 +30,4 @@ model = dict(
 work_dir = './work_dirs/char_detectors_htc'
 # fp16 = dict(loss_scale=512.)
 
-optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
