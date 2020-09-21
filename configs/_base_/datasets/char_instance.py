1f14045060dbdc5ede291bafcb6dc5c467e111e9
seoilgun <seoilgun@gmail.com>
2020-09-21 23:09:46 +0900

    ~

--- configs/_base_/datasets/char_instance.py
+++ configs/_base_/datasets/char_instance.py
@@ -32,7 +32,7 @@ classes=('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H'
 
 data = dict(
     samples_per_gpu=2,
-    workers_per_gpu=2,
+    workers_per_gpu=0,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'char/train_coco.json',
