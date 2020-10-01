ea93ec9c572e6daec5580f09ea29468189b8a420
seoilgun <seoilgun@gmail.com>
2020-10-02 00:12:28 +0900

    ~

--- configs/htc/htc_without_semantic_r50_fpn_1x_char.py
+++ configs/htc/htc_without_semantic_r50_fpn_1x_char.py
@@ -217,7 +217,7 @@ test_cfg = dict(
     rcnn=dict(
         score_thr=0.001,
         nms=dict(type='nms', iou_threshold=0.5),
-        max_per_img=200,
+        max_per_img=1,
         mask_thr_binary=0.5))
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
