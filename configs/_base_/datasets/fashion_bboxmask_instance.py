0105e33044d49f9b40898d716a779418cdfd53dd
seoilgun <seoilgun@gmail.com>
2020-11-08 02:34:31 +0900

    ~

--- configs/_base_/datasets/fashion_bboxmask_instance.py
+++ configs/_base_/datasets/fashion_bboxmask_instance.py
@@ -64,7 +64,7 @@ data = dict(
         classes=classes, pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'fashion/test_public.json',
+        ann_file=data_root + 'fashion/test_pubilc.json',
         img_prefix=data_root + 'fashion/test_images',
         classes=classes, pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='bbox')
