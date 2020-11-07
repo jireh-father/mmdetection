a839f1acaaf1de00375e57adbe5997dc05486f88
seoilgun <seoilgun@gmail.com>
2020-11-08 02:58:54 +0900

    ~

--- configs/fashion_bboxmask_detectors.py
+++ configs/fashion_bboxmask_detectors.py
@@ -34,3 +34,17 @@ model = dict(
 work_dir = './work_dirs/fashion_detectors'
 
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
