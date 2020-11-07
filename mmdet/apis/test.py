306655e429a976f06f04044e3835f642615311dc
seoilgun <seoilgun@gmail.com>
2020-11-08 03:46:23 +0900

    ~

--- mmdet/apis/test.py
+++ mmdet/apis/test.py
@@ -24,7 +24,6 @@ def single_gpu_test(model,
     for i, data in enumerate(data_loader):
         with torch.no_grad():
             result = model(return_loss=False, rescale=True, **data)
-            print(result)
         batch_size = len(result)
         if show or out_dir:
             if batch_size == 1 and isinstance(data['img'][0], torch.Tensor):
