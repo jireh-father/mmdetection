ad42fc229cc1a12523c7ee740379c37b3562241d
seoilgun <seoilgun@gmail.com>
2020-11-08 03:06:16 +0900

    ~

--- tools/test.py
+++ tools/test.py
@@ -184,6 +184,7 @@ def main():
         outputs = multi_gpu_test(model, data_loader, args.tmpdir,
                                  args.gpu_collect)
 
+    print(outputs)
     rank, _ = get_dist_info()
     if rank == 0:
         if args.out:
