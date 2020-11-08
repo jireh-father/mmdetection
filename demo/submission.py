6813a8f94b6fcb300b8471409bf2c2f2e6c48657
seoilgun <seoilgun@gmail.com>
2020-11-08 16:40:55 +0900

    ~

--- demo/submission.py
+++ demo/submission.py
@@ -119,7 +119,7 @@ def main():
             new_polys = []
             for poly in polys:
                 new_poly = []
-                for j in range(0, len(poly), 6):
+                for j in range(0, len(poly), 10):
                     new_poly.append(poly[j])
                     new_poly.append(poly[j + 1])
                 new_polys.append(new_poly)
