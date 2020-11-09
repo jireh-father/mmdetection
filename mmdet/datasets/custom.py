1ffab921d0d68a6f22c17cb36eccdc08378cce1c
seoilgun <seoilgun@gmail.com>
2020-11-09 12:52:20 +0900

    ~

--- mmdet/datasets/custom.py
+++ mmdet/datasets/custom.py
@@ -189,6 +189,7 @@ class CustomDataset(Dataset):
                 if data is None:
                     idx = self._rand_another(idx)
                     continue
+                print("done")
                 return data
             except:
                 idx = self._rand_another(idx)
