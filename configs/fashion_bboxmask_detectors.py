c0077eaf069fcebc55fbac1e05f93778f66114b4
seoilgun <seoilgun@gmail.com>
2020-11-10 10:42:44 +0900

    ~

--- configs/fashion_bboxmask_detectors.py
+++ configs/fashion_bboxmask_detectors.py
@@ -34,19 +34,26 @@ model = dict(
 
 work_dir = './work_dirs/fashion_detectors'
 
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
-
+# optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.004, momentum=0.9, weight_decay=0.0001)
 # optimizer
 # optimizer = dict(type='SGD', lr=0.008, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
+# lr_config = dict(
+#     policy='step',
+#     warmup='linear',
+#     warmup_iters=500,
+#     warmup_ratio=0.001,
+#     step=[20, 23])
+# total_epochs = 24
 lr_config = dict(
     policy='step',
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.001,
-    step=[20, 23])
-total_epochs = 24
+    step=[28, 32])
+total_epochs = 36
 
 # checkpoint_config = dict(interval=1)
 # # yapf:disable
