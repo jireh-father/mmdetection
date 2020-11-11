45701a341058f39c811f4ec7008908a0bf8c7c1a
seoilgun <seoilgun@gmail.com>
2020-11-11 10:05:39 +0900

    ~

--- configs/fashion_bboxmask_detectors.py
+++ configs/fashion_bboxmask_detectors.py
@@ -38,7 +38,7 @@ work_dir = './work_dirs/fashion_detectors'
 optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 # optimizer
 # optimizer = dict(type='SGD', lr=0.008, momentum=0.9, weight_decay=0.0001)
-optimizer_config = dict(grad_clip=5.0)
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 # lr_config = dict(
 #     policy='step',
