276d74ce098564f0a26eac87083631de408a4544
seoilgun <seoilgun@gmail.com>
2020-11-17 20:24:15 +0900

    ~

--- configs/fashion_bboxmask_detectors.py
+++ configs/fashion_bboxmask_detectors.py
@@ -136,6 +136,80 @@ test_cfg = dict(
     #     mask_thr_binary=0.45)
 )
 
+dataset_type = 'FashionDataset'
+data_root = 'data/coco/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='Resize', img_scale=(800, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(800, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+# classes = ('top',
+#            'blouse',
+#            't-shirt',
+#            'Knitted fabric',
+#            'shirt',
+#            'bra top',
+#            'hood',
+#            'blue jeans',
+#            'pants',
+#            'skirt',
+#            'leggings',
+#            'jogger pants',
+#            'coat',
+#            'jacket',
+#            'jumper',
+#            'padding jacket',
+#            'best',
+#            'kadigan',
+#            'zip up',
+#            'dress',
+#            'jumpsuit')
+
+data = dict(
+    samples_per_gpu=3,
+    workers_per_gpu=3,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'fashion/train_total.json',
+        img_prefix=data_root + 'fashion/train_images',
+        # classes=classes,
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'fashion/val_split.json',
+        img_prefix=data_root + 'fashion/train_images',
+        # classes=classes,
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'fashion/test_pubilc.json',
+        img_prefix=data_root + 'fashion/test_images',
+        # classes=classes,
+        pipeline=test_pipeline))
+evaluation = dict(metric=['bbox', 'segm'])
 
 work_dir = './work_dirs/fashion_detectors'
 
