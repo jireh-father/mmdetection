f44bc6d818f65da89a2aca248cf0838790b640a1
seoilgun <seoilgun@gmail.com>
2020-11-19 02:21:32 +0900

    ~

--- demo/ensemble_submission.py
+++ demo/ensemble_submission.py
@@ -7,6 +7,7 @@ import pandas as pd
 from argparse import ArgumentParser
 import os
 import cv2
+from sklearn.metrics import jaccard_score
 
 
 def rle_encode(mask):
@@ -92,9 +93,11 @@ def mask_to_poly(mask):
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('--result_file', help='Image file')
+    parser.add_argument('--result_files', help='Image file')
     parser.add_argument('--output_dir')
     parser.add_argument('--thr', type=float, default=0.0)
+    parser.add_argument('--iou_thr', type=float, default=0.5)
+    parser.add_argument('--use_merge', action='store_true', default=False)
     parser.add_argument(
         '--device', default='cuda:0', help='Device used for inference')
     parser.add_argument(
@@ -103,6 +106,7 @@ def main():
 
     os.makedirs(args.output_dir, exist_ok=True)
 
+    result_files = args.result_files.split(",")
     filepath = args.result_file
     with open(filepath) as json_file:
         json_data = json.load(json_file)
@@ -114,31 +118,77 @@ def main():
     category_ids = []
     thr = 800 * 800 * args.thr
     skip_cnt = 0
-    for i in tqdm(range(len(json_data))):
-        # try:
-        #     mask = mutils.decode(json_data[i]['segmentation'])
-        #     polys = mask_to_poly(mask)
-        #     new_polys = []
-        #     for poly in polys:
-        #         new_poly = []
-        #         for j in range(0, len(poly), 100):
-        #             new_poly.append(poly[j])
-        #             new_poly.append(poly[j + 1])
-        #         new_polys.append(new_poly)
-        #
-        #     mask = annToMask(new_polys, json_data[i]['segmentation']['size'][0], json_data[i]['segmentation']['size'][1])
-        #     encoded_pixels.append(rle_to_string(rle_encode(mask)))
-        #
-        # except:
-        if mutils.decode(json_data[i]['segmentation']).sum() < thr:
-            skip_cnt += 1
-            print("skip")
-            continue
-        encoded_pixels.append(rle_to_string(rle_encode(mutils.decode(json_data[i]['segmentation']))))
-        img_ids.append(json_data[i]['image_id'])
-        category_ids.append(json_data[i]['category_id'])
-        height.append(json_data[i]['segmentation']['size'][0])
-        width.append(json_data[i]['segmentation']['size'][1])
+    total_data = {}
+    for ridx, result_file in enumerate(result_files):
+        json_data = json.load(open(result_file))
+        for i in tqdm(range(len(json_data))):
+            image_id = json_data[i]['image_id']
+            if image_id not in total_data:
+                total_data[image_id] = []
+            json_data[i]['ridx'] = ridx
+            total_data[image_id].append(json_data[i])
+
+            # if mutils.decode(json_data[i]['segmentation']).sum() < thr:
+            #     skip_cnt += 1
+            #     print("skip")
+            #     continue
+
+    for image_id in total_data:
+        if len(total_data[image_id]) == 1:
+            seg_result = total_data[image_id][0]
+            encoded_pixels.append(rle_to_string(rle_encode(seg_result['segmentation'])))
+            img_ids.append(image_id)
+            category_ids.append(seg_result['category_id'])
+            height.append(seg_result['segmentation']['size'][0])
+            width.append(seg_result['segmentation']['size'][1])
+        else:
+            tmp_data = total_data[image_id]
+            overlap_masks = []
+            overlap_mask_ids = set()
+            for i in range(len(tmp_data)):
+                for j in range(len(tmp_data)):
+                    if i == j:
+                        continue
+                    mask1 = mutils.decode(tmp_data[i]['segmentation'])
+                    mask2 = mutils.decode(tmp_data[j]['segmentation'])
+                    iou = jaccard_score(mask1, mask2)
+                    if iou >= args.iou_thr:
+                        overlap_mask_ids.add(i)
+                        overlap_mask_ids.add(j)
+                        overlap_masks.append(
+                            [[i, mask1], [j, mask2]])
+            single_mask_ids = set(range(len(tmp_data))) - overlap_mask_ids
+            used_masks = []
+            for mask_item1, mask_item2 in overlap_masks:
+                if mask_item1[0] in used_masks or mask_item2[0] in used_masks:
+                    continue
+                if args.use_merge:
+                    result_mask = ((mask_item1[1] + mask_item2[1]) / 2).astype(np.uint8)
+                    selected_id = mask_item1[0]
+                else:
+                    if mask_item1[0] < mask_item2[0]:
+                        result_mask = mask_item1[1]
+                        selected_id = mask_item1[0]
+                    else:
+                        result_mask = mask_item2[1]
+                        selected_id = mask_item2[0]
+
+                used_masks.append(mask_item1[0])
+                used_masks.append(mask_item2[0])
+
+                encoded_pixels.append(rle_to_string(rle_encode(result_mask)))
+                img_ids.append(image_id)
+                category_ids.append(tmp_data[selected_id]['category_id'])
+                height.append(tmp_data[selected_id]['segmentation']['size'][0])
+                width.append(tmp_data[selected_id]['segmentation']['size'][1])
+            for i in single_mask_ids:
+                seg_result = tmp_data[i]
+                encoded_pixels.append(rle_to_string(rle_encode(seg_result['segmentation'])))
+                img_ids.append(image_id)
+                category_ids.append(seg_result['category_id'])
+                height.append(seg_result['segmentation']['size'][0])
+                width.append(seg_result['segmentation']['size'][1])
+
     data = {'ImageId': img_ids,
             'EncodedPixels': encoded_pixels,
             'Height': height,
