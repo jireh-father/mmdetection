65c6a11565f3f5fcad9e8eedcec06ef397b7d3d0
seoilgun <seoilgun@gmail.com>
2020-11-20 15:29:30 +0900

    ~

--- configs/fashion_bboxmask_detectors.py
+++ configs/fashion_bboxmask_detectors.py
@@ -124,9 +124,9 @@ test_cfg = dict(
     #         mask_thr_binary=0.5)
     rcnn=dict(
         # score_thr=0.6
-        score_thr=0.8,
-        # nms=dict(type='soft_nms', iou_threshold=0.5),
-        nms=dict(type='batched_nms'),#, iou_threshold=0.5),
+        score_thr=0.85,
+        nms=dict(type='soft_nms', iou_threshold=0.5),
+        # nms=dict(type='batched_nms'),#, iou_threshold=0.5),
         max_per_img=100,
         mask_thr_binary=0.5)
     # rcnn=dict(
