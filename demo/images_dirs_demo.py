f7f6196b5eef48b0f1abbc61c809e02b92da7618
seoilgun <seoilgun@gmail.com>
2021-04-19 23:20:23 +0900

    ~

--- demo/images_dirs_demo.py
+++ demo/images_dirs_demo.py
@@ -71,7 +71,7 @@ def main():
             print(time.time() - start)
             # show the results
             if len(result) < 1 or len(result[0]) < 1:
-                shutil.copy(img, os.path.join(args.output_dir, 'nodetected'))
+                shutil.copy(img, os.path.join(args.output_dir, os.path.basename(img_dir), 'nodetected'))
                 continue
 
             if not args.use_only_crop:
